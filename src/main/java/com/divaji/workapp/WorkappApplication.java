b2012ce4333c322aa8dc379d2c8ade44e84d1ef9
divaji <divaji190@gmail.com>
2023-03-26 11:42:33 +0530

    third commit

--- src/main/java/com/divaji/workapp/WorkappApplication.java
+++ src/main/java/com/divaji/workapp/WorkappApplication.java
@@ -1,5 +1,7 @@
 package com.divaji.workapp;
 
+import static org.mockito.ArgumentMatchers.intThat;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +11,17 @@ public class WorkappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkappApplication.class, args);
 	}
+
 	
 	public void push() {
 		int i=0;
 		String s="divaji";
 	}
+  
+	public void get() {
+		int i=10;
+		String  s="bannu";
+	}
+	
 
 }
